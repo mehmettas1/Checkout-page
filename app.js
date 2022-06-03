a1bd0b4d7fe2e6397c2f3f6b3a1ba8247f7abaeb
mehmet <tas349873@gmail.com>
2022-06-03 17:22:13 +0300

    js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-// const taxRate = 0.18;
-// const shippingPrice = 15.0;
+const taxRate = 0.18;
+const shippingPrice = 15.0;
 
 window.addEventListener("load", ()=>{
-    calculateCartTotal();
+  
     //set item to LocalStorage
-    // localStorage.setItem("taxRate", taxRate);
-    // localStorage.setItem("shippingPrice", shippingPrice);
+    localStorage.setItem("taxRate", taxRate);
+    localStorage.setItem("shippingPrice", shippingPrice);
 
      //set item to SessionStorage
     //  sessionStorage.setItem("taxRate", taxRate);
@@ -18,14 +18,18 @@ window.addEventListener("load", ()=>{
 let productsDiv = document.querySelector(".products");
 productsDiv.addEventListener(".click",(event) => {
     if(event.target.className == "minus") {
-        console.log("minus button clicked");
+        event.target.nextElementSibling.innerText--;
+        // console.log("minus button clicked");
     }
     else if(event.target.classList.contains("plus")) {
+        event.target.previousElementSibling.innerText++
         console.log("plus buttton clicked");
     }
     else if(event.target.classList.contains("remove-product")) {
-        console.log("remove button clicked");
-    else {
-        console.log("other element clicked");
+       event.target.parentElement.parentElement.parentElement.remove();
+       // console.log("remove button clicked");
+    }
+    else{
+        // console.log("other element clicked");
     }
 })
